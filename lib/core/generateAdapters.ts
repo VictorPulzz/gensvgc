27180bdf7fe90f4200f7a9a4d8578f6e2e74f286
Victor Razdorov <victor.razdorov@gmail.com>
2024-03-25 13:06:48 +0300

    fix: 🐛 fix path for svg inside adapter

--- lib/core/generateAdapters.ts
+++ lib/core/generateAdapters.ts
@@ -4,18 +4,23 @@ import path from "path";
 import { alerts, BuilderEnum, CoreOptions, FileObject } from "../shared";
 import { getGenericPaths } from "../shared/utils/getGenericPaths";
 
-function genSvgPath(relative: string, file: FileObject[string], vitePostfix: string) {
+function genSvgPath(
+  svgPath: string,
+  relative: string,
+  file: FileObject[string],
+  vitePostfix: string,
+) {
   /**
    * Add postfix for handle vite-plugin-svgr package
    */
-  const folderPathSvg = file.dir.split(path.sep).slice(1).join(path.sep);
-  return path.join(relative, folderPathSvg, file.base) + vitePostfix;
+  const removeRootPath = relative.replace(path.normalize(svgPath), "");
+  return path.join(removeRootPath, path.normalize(file.dir), file.base) + vitePostfix;
 }
 
 export async function generateDynamicAdapter(
   filesObject: FileObject,
   { adapterFilePath, relativeSvgPath, adapterTplPath }: ReturnType<typeof getGenericPaths>,
-  { typescript, builder }: CoreOptions,
+  { typescript, builder, svgPath }: CoreOptions,
 ) {
   const filesList = Object.entries(filesObject);
   const vitePostfix = builder === BuilderEnum.VITE ? "?react" : "";
@@ -28,10 +33,10 @@ export async function generateDynamicAdapter(
 
   const mappedSvgSwitch = filesList
     .map(([, values]) => {
-      const svgPath = genSvgPath(relativeSvgPath, values, vitePostfix);
+      const resultSvgPath = genSvgPath(svgPath, relativeSvgPath, values, vitePostfix);
       return `
     case "${values.fileName}":
-      svgComponent = await import("${svgPath}");
+      svgComponent = await import("${resultSvgPath}");
       break;`;
     })
     .join("");
@@ -59,15 +64,15 @@ export async function generateDynamicAdapter(
 export async function generateStaticAdapter(
   filesObject: FileObject,
   { adapterFilePath, relativeSvgPath, adapterTplPath }: ReturnType<typeof getGenericPaths>,
-  { builder }: CoreOptions,
+  { builder, svgPath }: CoreOptions,
 ) {
   const filesList = Object.entries(filesObject);
   const vitePostfix = builder === BuilderEnum.VITE ? "?react" : "";
 
   const mappedSvgImports = filesList
     .map(([, values], index) => {
-      const svgPath = genSvgPath(relativeSvgPath, values, vitePostfix);
-      return `import ${values.fileName} from "${svgPath}";${filesList.length - 1 !== index ? "\n" : ""}`;
+      const resultSvgPath = genSvgPath(svgPath, relativeSvgPath, values, vitePostfix);
+      return `import ${values.fileName} from "${resultSvgPath}";${filesList.length - 1 !== index ? "\n" : ""}`;
     })
     .join("");
 
